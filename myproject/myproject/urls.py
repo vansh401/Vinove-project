6df5908b6f148bf1190a85c609af490f46400d56
hansika1120 <137263599+hansika1120@users.noreply.github.com>
2024-08-31 16:57:12 +0530

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
     path('activity_tracker/', include('activity_tracker.urls')),
 ]
 
